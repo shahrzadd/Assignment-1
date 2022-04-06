f980417508427acd6ce52bd6dfee4c29332b30bc
shahrzadd <shahrzadd@gmail.com>
2022-04-05 20:58:51 -0400

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,10 @@
+module.exports = function (config) {
+  config.addPassthroughCopy('src/js');
+  config.addPassthroughCopy('src/css');
+  return {
+    dir: {
+      input: 'src',
+      output: 'dist',
+    },
+  };
+};
